121929b8d719cac8e7d11cf825e588518e8b1a0c
Cosmin Stroe <cstroe@users.sf.net>
2016-04-29 19:36:06 -0500

    Remove mentions to old repositories.

--- src/main/java/com/github/cstroe/sqs/repository/RepositoryFactory.java
+++ src/main/java/com/github/cstroe/sqs/repository/RepositoryFactory.java
@@ -1,10 +1,4 @@
 package com.github.cstroe.sqs.repository;
 
 public class RepositoryFactory {
-    public static NotebookRepository notebook() {
-        return new NotebookRepository();
-    }
-    public static NoteRepository note() {
-        return new NoteRepository();
-    }
 }
